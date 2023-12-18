8ea3901dcbc44d9c8a42592008b8094fa5c612cb
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-18 15:45:02 +0200

    made file input button an image with nicer look instead of plain field

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,4 +1,7 @@
+
+
 const Register = () => {
+    
     return (
         <div className="formContainer">
             <div className="formWrapper">
@@ -8,7 +11,11 @@ const Register = () => {
                     <input type="text" placeholder="display name" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
-                    <input type="file" />
+                    <input style={{display: "none"}} type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src="https://www.shareicon.net/data/512x512/2016/06/30/788846_add_512x512.png" alt=""></img>
+                        <span>Add an avatar</span>
+                    </label>
                     <button>Sign up</button>
                 </form>
                 <p>You do have an account ? Login</p>
