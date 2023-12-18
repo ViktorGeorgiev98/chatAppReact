87ea590df38d9cf0e3e1707ba78220d3052b4241
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-18 15:17:12 +0200

    start creating register page

---
+++ client/src/components/Register/Register.jsx
@@ -0,0 +1,12 @@
+const Register = () => {
+    return (
+        <div className="form-container">
+            <div className="formWrapper">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Register;
