121c530be1da928561b25f94153cb81720eb288a
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-18 15:20:51 +0200

    wrote the start of the form for register

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -2,7 +2,16 @@ const Register = () => {
     return (
         <div className="form-container">
             <div className="formWrapper">
-                
+                <span className="logo">React chat app</span>
+                <span className="title">Register</span>
+                <form>
+                    <input type="text" placeholder="display name" />
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="password" />
+                    <input type="file" />
+                    <button>Sign up</button>
+                </form>
+                <p>You do have an account ? Login</p>
             </div>
 
         </div>
