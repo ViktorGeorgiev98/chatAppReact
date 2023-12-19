b7793ada3e8c3a896fbe0451c95cf6269d9e8d60
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-19 17:25:32 +0200

    styling for the input component, also added images

--- client/src/components/Home/Input.jsx
+++ client/src/components/Home/Input.jsx
@@ -1,7 +1,17 @@
 const Input = () => {
     return (
         <div className="input">
-            input
+            <input type="text" placeholder="Type something..."></input>
+            <div className="send">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Ic_attach_file_48px.svg/1024px-Ic_attach_file_48px.svg.png" alt="" />
+                <input type="file" style={{display: "none"}} id="file" />
+                <label htmlFor="file">
+                    <img src="https://www.creativefabrica.com/wp-content/uploads/2021/08/07/Image-Gallery-Icon-Graphics-15590538-1-580x386.jpg" alt="" />
+                </label>
+                <button>
+                    Send
+                </button>
+            </div>
         </div>
     )
 }
