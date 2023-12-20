b7a68c64792b5fc3478238fc4d781e834f4da5d1
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-20 16:31:35 +0200

    start of firebase config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@
 import '../src/style.scss'
 import Home from './components/Home/Home'
 // import Login from './components/Login/Login'
-// import Register from './components/Register/Register'
+import Register from './components/Register/Register'
 
 function App() {
  
 
   return (
     <>
-    <Home />
-     {/* <Register /> */}
+    {/* <Home /> */}
+     <Register />
      {/* <Login /> */}
     </>
   )
