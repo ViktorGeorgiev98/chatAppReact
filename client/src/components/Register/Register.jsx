6fafbfe7ffe5a0f195baf71ac2da4bfeb75e4f51
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-20 16:58:41 +0200

    added storage method to firebase config file

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -24,16 +24,7 @@ const Register = () => {
             return alert(e.message);
         }
 
-        // createUserWithEmailAndPassword(auth, email, password)
-        // .then((userCredential) => {
-        //     const user = userCredential.user;
-        //     console.log({user});
-        // })
-        // .catch((error) => {
-        //     const errorCode = error.code;
-        //     const errorMessage = error.message;
-        //     return alert(errorMessage);
-        // })
+       
 
 
     }
--- client/src/firabase.js
+++ client/src/firabase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDfiin6mXTrl7xgl2mX6i4svBl6PAXxizc",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
