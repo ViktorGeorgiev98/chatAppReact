1279eb952ea35732af89fa1150e42ebe8192c118
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-20 16:44:44 +0200

    added register function on submit form

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 
 const Register = () => {
     const [displayName, setDisplayName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        })
+
+
+    }
     
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">React chat app</span>
                 <span className="title">Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="display name" onChange={(e) => setDisplayName(e.currentTarget.value)} />
                     <input type="email" placeholder="email" onChange={(e) => setEmail(e.currentTarget.value)} />
                     <input type="password" placeholder="password" onChange={(e) => setPassword(e.currentTarget.value)} />
