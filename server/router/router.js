dae2bbcf618a856a5b88fa871223a7bd5fa648c2
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-22 16:12:18 +0200

    created user router

--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const userRouter = require('../router/userRouting');
 
 router.get('/', (req, res) => {
     res.send("Hello from server!");
 })
 
+router.use('/users', userRouter);
 
 router.get('*', (req, res) => {
     res.send("Page not found!");
