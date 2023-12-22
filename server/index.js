f47cf5c064aaa4b586ea893741e14651cc5891fd
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-22 17:32:46 +0200

    added cors to the app

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const dbConfig = require("./database/dbConfig");
 const router = require("./router/router");
 const PORT = 3030;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 // Initialization of express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:5173' // Replace with your frontend origin
+}));
 
 app.use(router);
 
