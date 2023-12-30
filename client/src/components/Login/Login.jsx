1f792c9d300423d8dcf0c84e30d2d671298bbb7f
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-30 17:08:14 +0200

    started creating login submit handler

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -7,6 +7,11 @@ const Login = () => {
 
     async function submitLoginHandler(e) {
         e.preventDefault();
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        console.log({email, password});
     }
 
     
@@ -16,8 +21,8 @@ const Login = () => {
                 <span className="logo">React chat app</span>
                 <span className="title">Login</span>
                 <form onSubmit={submitLoginHandler}>
-                    <input onChange={(e) => setEmail(e.currentTarget.value)} type="email" placeholder="email" />
-                    <input onChange={(e) => setPassword(e.currentTarget.value)} type="password" placeholder="password" />
+                    <input onChange={(e) => setEmail(e.currentTarget.value)} type="email" placeholder="email" name="email" />
+                    <input onChange={(e) => setPassword(e.currentTarget.value)} type="password" placeholder="password" name="password" />
                     <button>Sign up</button>
                 </form>
                 <p>You don't have an account ? Register</p>
