504a91ccb2237833a8d5b29e58e67d42adebbe7f
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-30 16:28:45 +0200

    isntalled routes and added some routes to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 
 import '../src/style.scss'
 import Home from './components/Home/Home'
-// import Login from './components/Login/Login'
+import Login from './components/Login/Login'
 import Register from './components/Register/Register'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
  
 
   return (
     <>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route  path='/home' element={<Home />} />
+    </Routes>
     {/* <Home /> */}
-     <Register />
-     {/* <Login /> */}
     </>
   )
 }
