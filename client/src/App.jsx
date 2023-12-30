a8d52afbd732026076a10c6a1c872424b5e5671d
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-30 16:53:43 +0200

    added auth guard and guest guard to app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import NotFound from './components/NotFound/NotFound';
 import Register from './components/Register/Register'
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
+import AuthGuard from './routerGuards/AuthGuard';
+import GuestGuard from './routerGuards/GuestGuard';
 
 function App() {
  
@@ -14,9 +16,13 @@ function App() {
     <>
     <AuthProvider>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route  path='/home' element={<Home />} />
+        <Route element={<AuthGuard />}>
+          <Route  path='/home' element={<Home />} />
+        </Route>
+        <Route element={<GuestGuard />}>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </AuthProvider>
