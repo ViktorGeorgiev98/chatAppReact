e635450fe99ef96f2c3e1f72acdab918b94371f0
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-30 16:41:34 +0200

    created auth context and added it to app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import Login from './components/Login/Login'
 import NotFound from './components/NotFound/NotFound';
 import Register from './components/Register/Register'
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
  
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route  path='/home' element={<Home />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route  path='/home' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
+    
     </>
   )
 }
---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { createContext, useContext, useState, useEffect } from "react"
+
+const AuthContext = createContext();
+
+
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+}
+
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    const logout = () => {
+        setUser(previousUser => ({}));
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+    }
+
+    const login = (userData) => {
+       
+        setUser(previousUser => ({
+            username: userData.username,
+            email: userData.email,
+            password: userData.password,
+            accessToken: userData.accessToken,
+            _id: userData._id
+        }));
+        localStorage.setItem('user', JSON.stringify(userData));
+        localStorage.setItem('token', userData.accessToken)
+    }
+
+    const isAuthenticated = () => {
+        const token = localStorage.getItem('token');
+        console.log({token: token}); // Check what value is retrieved from localStorage
+        if (!!token && !!user.accessToken) {
+            return true;
+        } else {
+            return false;
+        }
+        // return !!user.accessToken;
+    }
+
+    const getAccessToken = () => {
+        return localStorage.getItem("token");
+    }
+
+    const getUsername = () => {
+        return user.username
+    }
+
+    const authContextValue = {
+        getUsername,
+        user,
+        login,
+        logout,
+        isAuthenticated,
+        getAccessToken
+    };
+
+    return (
+        <AuthContext.Provider value={authContextValue}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
