e7b717f78d83d1ae274e92839892247e84cc2e4a
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-12-30 17:06:26 +0200

    made login component form controlled

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function submitLoginHandler(e) {
+        e.preventDefault();
+    }
+
     
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">React chat app</span>
                 <span className="title">Login</span>
-                <form>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
+                <form onSubmit={submitLoginHandler}>
+                    <input onChange={(e) => setEmail(e.currentTarget.value)} type="email" placeholder="email" />
+                    <input onChange={(e) => setPassword(e.currentTarget.value)} type="password" placeholder="password" />
                     <button>Sign up</button>
                 </form>
                 <p>You don't have an account ? Register</p>
