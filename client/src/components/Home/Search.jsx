4081ce59897b0f0eb86aea58b976404061a6df55
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-04 15:49:12 +0200

    started creating search user method

--- client/src/components/Home/Search.jsx
+++ client/src/components/Home/Search.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 const Search = () => {
+    const [username, setUsername] = useState('');
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState(false);
+
+    const handleSearch = async () => {
+        try {
+            const response = await fetch('');
+        } catch(e) {
+            console.log(e.message);
+            return alert(e.message);
+        }   
+    }
+
+    const handleKeydown = async (e) => {
+        e.preventDefault();
+        e.code === 'Enter' && handleSearch();
+    }
     return (
         <div className="search">
             <div className="searchForm">
-                <input type="text" placeholder="find a user" />
+                <input type="text" placeholder="find a user" onKeyDown={handleKeydown} onChange={(e) => setUsername(e.currentTarget.value)} />
             </div>
             <div className="userChat">
                 <img src="https://www.mypokecard.com/en/Gallery/my/galery/m3dsSJwTlM8W.jpg" alt="" />
