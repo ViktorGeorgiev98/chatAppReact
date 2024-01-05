12e230f4507f1a52b70ed817a7a758795a209fb7
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-05 13:40:42 +0200

    created some needed services functions for chat room

---
+++ server/services/chatRoomServices.js
@@ -0,0 +1,28 @@
+const chatRoomModel = require('../database/User');
+
+
+chatRoomModel.createChatRoom = async (roomId, participant1, participant2) => {
+    return await chatRoomModel.create({roomId, participant1, participant2});
+}
+
+chatRoomModel.findByRoomId = async (roomId) => {
+    return await chatRoomModel.find({roomId: roomId})
+}
+
+chatRoomModel.addMessageFromUser = async (user, message) => {
+    room.participants.find(participant => participant.user.equals(user))
+      .messages.push({
+        sender: user,
+        message: message
+      });
+}
+
+chatRoomModel.getAllMessagesForBothUsers = async (user1, user2) => {
+    const user1Messages = await chatRoomModel.find({participantId: user1}).messages;
+    const user2Messages = await chatRoomModel.find({participantId: user2}).messages;
+    const allMessages = {user1Messages: user1Messages, user2Messages: user2Messages};
+    return allMessages;
+}
+
+
+module.exports = chatRoomModel;
